b434948a7a86694576278348543d10950d02c896
Alexiv90 <alex.ivanchenko@tele2.ru>
2023-08-29 20:16:28 +0000

    Add terraform-2 HW

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -4,11 +4,9 @@ provider "yandex" {
   folder_id                = var.folder_id
   zone                     = var.zone
 }
-
 module "app" {
   source          = "../modules/app"
   public_key_path = var.public_key_path
-  private_key_path = var.private_key_path
   app_disk_image  = var.app_disk_image
   subnet_id       = var.subnet_id
 }
@@ -16,7 +14,10 @@ module "app" {
 module "db" {
   source          = "../modules/db"
   public_key_path = var.public_key_path
-  private_key_path = var.private_key_path
   db_disk_image   = var.db_disk_image
   subnet_id       = var.subnet_id
+}
+
+module "vpc" {
+  source          = "../modules/vpc"
 }
--- terraform/prod/variables.tf
+++ terraform/prod/variables.tf
@@ -28,6 +28,6 @@ variable "app_disk_image" {
   default     = "reddit-app-base"
 }
 variable "db_disk_image" {
-  description = "Disk image for reddit db"
-  default     = "reddit-db-base"
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
 }
